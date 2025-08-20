4f5bb792b577066bc7efb7f8b80702746c8548d5
JayPadghan <padghanjay911@gmail.com>
2025-08-21 00:47:26 +0530

    7 webContainer and terminal

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,27 @@ const nextConfig: NextConfig = {
         pathname: '/**'
       }
     ]
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin'
+          },
+          {
+            key: 'Cross-Origin-Embedder-Policy',
+            value: 'require-corp'
+          },
+          // {
+          //   key: 'Cross-Origin-Resource-Policy',
+          //   value: 'cross-origin'
+          // }
+        ]
+      }
+    ]
   }
 };
 
